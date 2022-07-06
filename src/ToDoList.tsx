7a7162c603195c44a09492cb8995216267904f7e
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-06 17:28:33 +0900

    toDoList - 1
    recoil사용
    useRecoilState를 사용한 생성 및 수정
    interface를 통한 type 지정
    ...prev 를 통해 []안의 요소만 생성

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,98 +1,39 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
-
-// function ToDoList() {
-//     const [toDo, setToDo] = useState("");
-
-//     const onchange = (e: React.FormEvent<HTMLInputElement>) => {
-//         const { currentTarget: { value } } = e;
-//         setToDo(value);
-//     }
-
-//     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//         e.preventDefault();
-//     if (toDo.length < 10) {
-//     return setToDoError("To do should be longer");
-//       }
-//         console.log(toDo);
-//         setToDo("");
-//     }
-//     return (
-//         <>
-//             <form onSubmit={onSubmit}>
-//                 <input value={toDo} onChange={onchange} placeholder="Write To Do"></input>
-//                 <button>Submit</button>
-//             </form>
-//         </>
-//     );
-// }
+import { atom, useRecoilState } from "recoil";
 
 interface IForm {
-    email: string;
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    password1: string;
-    extraError?: string;
+    toDo: string;
 }
 
+interface IToDo {
+    text: string;
+    id: number;
+    catergory: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoAtom = atom<IToDo[]>({
+    key: "toDo",
+    default: [],
+})
 function ToDoList() {
-    const { register, watch, handleSubmit, formState: { errors }, setError } = useForm<IForm>({ defaultValues: { email: "@naver.com" } });
 
-    const onValid = (data: IForm) => {
-
-        if (data.password !== data.password1) {
-            setError("password1", { message: "password not correct" }, { shouldFocus: true });
-        }
-        // server가 off와 같이 문제 발생시 사용 가능.  setError("extraError", { message: "Server Off" })
+    const { register, handleSubmit, setValue } = useForm<IForm>();
+    const [toDos, setToDos] = useRecoilState<IToDo[]>(toDoAtom);
+    const onValid = ({ toDo }: IForm) => {
+        setToDos((prev) => [{ text: toDo, id: Date.now(), catergory: "TO_DO" }, ...prev])
+        setValue("toDo", "");
     }
 
     return (
-        <div>
-            <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
-
-                <input {...register("email", {
-                    required: "Write Email", pattern: {
-                        value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                        message: "Only @naver.com allowed",
-                    },
-                    validate: {
-                        dog: (value) => value.includes("dog") ? "No Allow Dog" : true,
-                        cat: (v) => v.includes("cat") ? "No Allow Cat" : true
-                    }
-                })} placeholder="Email" />
-                <span>{errors?.email?.message}</span>
-
-                <input {...register("firstName", { required: "Write Plz" })} placeholder="First Name" />
-                <span>{errors?.firstName?.message}</span>
-
-                <input {...register("lastName", { required: "Write Plz" })} placeholder="Last Name" />
-                <span>{errors?.lastName?.message}</span>
-
-                <input {...register("username", { required: "Write Plz" })} placeholder="Username" />
-                <span>{errors?.username?.message}</span>
-
-                <input {...register("password", {
-                    required: true,
-                    minLength: {
-                        value: 5,
-                        message: "Your Password is too short"
-                    }
-                })} placeholder="Password" />
-                <span>{errors?.password?.message}</span>
-                <input {...register("password1", {
-                    required: "Write Plz", minLength: {
-                        value: 5,
-                        message: "Your Password1 is too short"
-                    }
-                })} placeholder="Password1" />
-                <span>{errors?.password1?.message}</span>
+        <>
+            <form onSubmit={handleSubmit(onValid)}>
+                <input {...register("toDo", { required: "Write To Do" })} placeholder="Write To Do"></input>
                 <button>Add</button>
-                <span>{errors?.extraError?.message}</span>
             </form>
-        </div>
-    );
+            <ul>
+                {toDos.map((toDo) => <li key={toDo.id}>{toDo.text}</li>)}
+            </ul>
+        </>
+    )
 }
 export default ToDoList;
