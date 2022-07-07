9885bff7ec0ddb33c523283eeda259bc5621e1a2
Chi Hwan Kim <rlaclghks123@naver.com>
2022-07-07 16:36:09 +0900

    ToDo - category
    name을 통해 어떤 button이 클릭되는지 확인 가능
    
    다른 방법으론 //처리 된 부분

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,27 @@
+import React from "react";
 import { IToDo } from "../atoms";
 
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, catergory }: IToDo) {
+
+    // const onclick = (newCategory : IToDo["category"]) =>{}
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const { currentTarget: { name } } = event;
+        console.log(name);
+    }
+
     return (<li>
         <span>{text}</span>
-        <button>To Do</button>
-        <button>Doing</button>
-        <button>Done</button>
+
+        {/* {catergory !== "TO_DO" && <button   onClick={()=>onClick("TO_DO"))}>To Do</button>}
+        {catergory !== "DOING" &&     <button   onClick={()=>onClick("DOING"))}>DOING</button>}
+        {catergory !== "DONE" &&      <button  onClick={()=>onClick("DONE"))}>DONE</button>}
+         */}
+
+        {catergory !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
+        {catergory !== "DOING" && <button name="DOING" onClick={onClick}>DOING</button>}
+        {catergory !== "DONE" && <button name="DONE" onClick={onClick}>DONE</button>}
+
     </li>);
 }
 export default ToDo;
